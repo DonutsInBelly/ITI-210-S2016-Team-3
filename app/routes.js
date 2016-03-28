d84810c8f5ede1460c0c142f1b164b155ea0b5cf
Carlin <carlinau@gmail.com>
2016-03-28 10:30:45 -0400

    working on account system

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,35 @@ module.exports = function(app)
 {
   app.get('/', function(req, res)
   {
-    res.sendfile('public_html/index.html');
+    res.sendfile('views/index.html');
   });
 
   app.get('/index', function(req, res)
   {
     res.redirect('/');
   });
-  
+
+  app.get('/login', function(req, res) {
+    res.render('login.html', {
+      message: req.flash('loginMessage')
+    });
+  });
+
+  app.get('/signup', function(req, res) {
+    res.render('profile.html', {
+      user: req.user
+    });
+  });
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
 }
--- server.js
+++ server.js
@@ -11,9 +11,23 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var configDB     = require('./config/database.js');
 
-app.use(express.static(__dirname + '/public_html'));
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+
 app.use(express.static(__dirname + '/views'));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'team3' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 require('./app/routes.js');
 app.listen(port);
